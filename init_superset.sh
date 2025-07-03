92797fb52af6266a959b28309584eb1de4572e6c
Belizario Rocha <belizariorocha@gmail.com>
2025-07-03 10:05:06 -0500

    Update init_superset.sh

--- init_superset.sh
+++ init_superset.sh
@@ -1,9 +1,24 @@
-superset db upgrade
+#!/bin/bash
+
+set -e
+
+# Wait for DB to be ready
+until superset db upgrade; do
+  echo "Waiting for database..."
+  sleep 5
+done
+
+# Create admin user
 export FLASK_APP=superset
 superset fab create-admin \
   --username admin \
   --firstname Superset \
   --lastname Admin \
   --email admin@example.com \
-  --password yourpassword
+  --password admin
+
+# Initialize roles, permissions, etc.
 superset init
+
+# Start Superset server
+exec superset run -h 0.0.0.0 -p 8088 --with-threads
